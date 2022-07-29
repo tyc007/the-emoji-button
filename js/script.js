913611cfc9d26915f7f371d0e54053f704f532d5
tyc007 <tonychan550@gmail.com>
2022-07-29 11:28:09 -0700

    Added function that gets difference between codepoints

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
+const EMOJI_STARTING_UNICODE = '1F600'
+const EMOJI_ENDING_UNICODE = '1F64F'
+ 
+
 // Returns a random number between 0 and max number provided
 function getRandomNumber(max) {
     return Math.floor(Math.random() * max)
 }
+
+function getUnicodeFromCodePoint(codePoint) {
+    return String.fromCodePoint(parseInt(codePoint,16));
+}
+
+function getCodePointDifference(a,b){
+    return (parseInt(a,16) - parseInt(b,16)) + 1
+}
+
+console.log(getUnicodeFromCodePoint('1F64F'))
+console.log(getCodePointDifference(EMOJI_ENDING_UNICODE,EMOJI_STARTING_UNICODE))
