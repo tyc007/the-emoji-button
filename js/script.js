a9d4daea2faf6979bd656dfc5605564ac4384248
tyc007 <tonychan550@gmail.com>
2022-07-29 19:51:59 -0700

    added randomize logic to button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-const EMOJI_STARTING_UNICODE = '1F600'
-const EMOJI_ENDING_UNICODE = '1F64F'
-const EMOJI_CODEPOINT_ARRAY = []
-const EMOJI_ARRAY = []
+const EMOJI_STARTING_UNICODE = '1F600';
+const EMOJI_ENDING_UNICODE = '1F64F';
+const EMOJI_CODEPOINT_ARRAY = [];
+const EMOJI_ARRAY = [];
 
 function getRandomNumber(max) {
-    return Math.floor(Math.random() * max)
+    return Math.floor(Math.random() * max);
 }
 
 function getUnicodeFromCodePoint(codePoint) {
@@ -12,20 +12,33 @@ function getUnicodeFromCodePoint(codePoint) {
 }
 
 function getCodePointDifference(a,b){
-    return (parseInt(a,16) - parseInt(b,16)) + 1
+    return (parseInt(a,16) - parseInt(b,16)) + 1;
 }
 
 function populateEmojiList(){
     let emojiCodePoint = EMOJI_STARTING_UNICODE;
-    let emojiCodePointRange = getCodePointDifference(EMOJI_ENDING_UNICODE,EMOJI_STARTING_UNICODE)
+    let emojiCodePointRange = getCodePointDifference(EMOJI_ENDING_UNICODE,EMOJI_STARTING_UNICODE);
     for (let i = 0; i < emojiCodePointRange; i++) {
         EMOJI_CODEPOINT_ARRAY.push(emojiCodePoint);
-        EMOJI_ARRAY.push(getUnicodeFromCodePoint(emojiCodePoint))
+        EMOJI_ARRAY.push(getUnicodeFromCodePoint(emojiCodePoint));
         emojiCodePoint = (parseInt(emojiCodePoint, 16) + 1).toString(16).toUpperCase();
     }
 }
 
-populateEmojiList()
-let random = getRandomNumber(getCodePointDifference(EMOJI_ENDING_UNICODE,EMOJI_STARTING_UNICODE))
-document.getElementById("x").textContent = EMOJI_ARRAY[random];
-console.log((EMOJI_ARRAY))
+function randomizeEmoji(element){
+    let range = getCodePointDifference(EMOJI_ENDING_UNICODE,EMOJI_STARTING_UNICODE);
+    let random = getRandomNumber(range);
+    element.textContent = EMOJI_ARRAY[random];
+}
+
+populateEmojiList();
+let emojiElement = document.getElementById("emoji-display");
+randomizeEmoji(emojiElement);
+
+let theEmojiButton = document.getElementById("the-emoji-button");
+theEmojiButton.addEventListener("click", function(){
+    randomizeEmoji(emojiElement);
+}); 
+
+
+
